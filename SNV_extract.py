feae2ecc20c2a8d5b5681302d88bddd524dec9af
Alex <Kononov.nov@mail.ru>
2017-07-31 03:29:25 +0300

    add extracter for snv data

---
+++ SNV_extract.py
@@ -0,0 +1,60 @@
+
+# coding: utf-8
+
+# In[41]:
+
+import sys
+arg=sys.argv
+fst=0
+name_list=[]
+f_w=open(arg[1],'r')
+lable=f_w.readline().strip().split('\t')
+
+#create list of files
+for col in range(len(lable)):
+    if lable[col].strip('"')=='FS':
+        fst=col+1
+        continue
+    if fst!=0:
+        name_list.append(lable[col].strip('"').strip('.AD'))
+
+#clean list of files
+i=0
+while i<len(name_list):
+    if name_list[i].find('.DP')!=-1:
+        name_list.pop(i)
+        continue
+    i+=1
+
+#create files
+file_list=[]
+for i in name_list:
+    file_list.append(open(i+'.snv.txt','w'))
+
+
+for line in f_w:
+    lp=line.strip().split('\t')
+    i=fst
+    j=0
+    while i<=len(lp):
+        if lp[i+1]=='NA':
+            i+=2
+            j+=1
+            continue
+        print(name_list[j])
+        print(lp[i+1])
+        AD=lp[i].strip('"').split(',')
+        file_list[j].write(lp[0].strip('"')+'\t'+lp[1].strip('"')+'\t'+AD[1]+'\t'+lp[i+1]+'\n')
+        j+=1
+        i+=2
+
+#close files
+for i in file_list:
+    i.close()
+    
+
+
+# In[ ]:
+
+
+
