738ba63c8f960a45f6c5e9bc31eae05ee4705152
Alex <Kononov.nov@mail.ru>
2017-10-02 20:47:47 +0700

    Update filterByVariance.py

--- filterByVariance.py
+++ filterByVariance.py
@@ -1,50 +1,48 @@
 import sys
+import numpy as np
 
 arg=sys.argv
 
 #lable=True
+#arg[1]='H002_B_Statisticfiltered.vcf'
 f_cvf=open(arg[1], 'r')
-f_bam=open(arg[1]+'_baf11.txt', 'w')
-f_cnv=open(arg[1]+'_cnv11.txt', 'w')
-step=100
+f_bam=open(arg[1]+'_baf1000.txt', 'w')
+f_cnv=open(arg[1]+'_cnv1000.txt', 'w')
+step=1000
+number_of_position=0
 line='000'
+#while number_of_position < 300:
 while line!='':
-	data=[]
-	for j in range(step):
-		line=f_cvf.readline()
-		if line=='':
-			break
-		if line[0]=='#':
-			continue
-		#if lable:
-		#	lable=False
-		#	continue
-		colm=line.strip().split('\t')
-		if colm[6]!='PASS':
-			continue
-		colm[0]=colm[0].replace('chr','').replace('X', '23').replace('Y', '24').replace('M', '25')
-		rg=colm[-1].strip().split(':')
-		if len(rg)<=3:
-			continue
-		rb=rg[1].strip().split(',')
-		#print(line)
-		data.append([int(colm[0]),int(colm[1]),int(rb[1]),int(rg[2])])
-	if len(data)<=1:
-		continue
-	x=0
-	for i in data:
-		x+=i[3]
-	x=x/len(data)
-	SD=0
-	for i in data:
-		SD+=(i[3]**2 - x**2)
-	SD=SD/(len(data)-1)
-	down_lim=x-(1*SD)
-	up_lim=x+(1*SD)
-	for i in data:
-		if i[3] > down_lim and i[3] < up_lim:
-			f_bam.write((str(i[0])+'\t'+str(i[1])+'\t'+str(i[2]))+'\t'+str(i[3])+'\n')
-			f_cnv.write((str(i[0])+'\t'+str(i[1])+'\t'+str(i[3]))+'\t'+'1'+'\n')
+    data=[]
+    for j in range(step):
+        line=f_cvf.readline()
+        if line=='':
+            break
+        if line[0]=='#':
+            continue
+        colm=line.strip().split('\t')
+        if colm[6]!='PASS':
+            continue
+        colm[0]=colm[0].replace('chr','').replace('X', '23').replace('Y', '24').replace('M', '25')
+        rg=colm[-1].strip().split(':')
+        if len(rg)<=3:
+            continue
+        rb=rg[1].strip().split(',')
+        #print(line)
+        data.append([int(colm[0]),int(colm[1]),int(rb[1]),int(rg[2])])
+        number_of_position+=1
+    
+    if len(data)<=1:
+        continue
+    data=np.array(data)
+    mean=data[:,3].mean()
+    variance=data[:,3].var()
+    down_lim=mean-(2*variance)
+    up_lim=mean+(2*variance)
+    for i in data:
+        if i[3] > down_lim and i[3] < up_lim:
+            f_bam.write((str(i[0])+'\t'+str(i[1])+'\t'+str(i[2]))+'\t'+str(i[3])+'\n')
+            f_cnv.write((str(i[0])+'\t'+str(i[1])+'\t'+str(i[3]))+'\t'+'1'+'\n')
 
 f_cvf.close()
 f_cnv.close()
