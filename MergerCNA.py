339c1be76e9df853dac78642f368fa6a30bbe959
Kononov Alexander <alexkono@dh2-fimm84.helsinki.fi>
2017-10-10 16:45:32 +0300

    The sctipt creates overlapred files

--- MergerCNA.py
+++ MergerCNA.py
@@ -1,7 +1,13 @@
-a='H002_iAsc1_CL1_DNA1.bam.vcflogR.txt'
-b='H002_pOme1_CL12_DNA1.bam.vcflogR.txt'
-doc1=open(a,'r')
-doc2=open(b,'r')
+import sys 
+
+arg=sys.argv
+
+#a='H002_iAsc1_CL1_DNA1.bam.vcflogR.txt'
+#b='H002_pOme1_CL12_DNA1.bam.vcflogR.txt'
+doc1=open(arg[1],'r')
+doc2=open(arg[2],'r')
+doc1_w=open(arg[1]+'OL.txt','w')
+doc2_w=open(arg[2]+'OL.txt','w')
 i1=4
 i2=4
 coint=0
@@ -11,7 +17,8 @@ line2=doc2.readline()
 lpr2=line2.strip().split('\t')
 while 0==0 :
 	if line1=='' or line2=='':
-		break		
+		break
+#check the position and write overlapped position
 	if lpr1[0]==lpr2[0]:
 		if int(lpr1[1])<int(lpr2[1]):
 			line1=doc1.readline()
@@ -22,24 +29,28 @@ while 0==0 :
 			lpr2=line2.strip().split('\t')
 			continue
 		coint+=1
+		doc1_w.write(line1)
+		doc2_w.write(line2)
 		line1=doc1.readline()
 		lpr1=line1.strip().split('\t')
-		line2=doc2.readline()
-		lpr2=line2.strip().split('\t')
-		continue
-	#print(lpr1)
-	#print(lpr2)
-	if int(lpr1[0].replace('X','23').replace('Y','24').replace('M','25'))<int(lpr2[0].replace('X','23').replace('Y','24').replace('M','25')):
-		line1=doc1.readline()
-		lpr1=line1.strip().split('\t')
-		continue
-	if int(lpr2[0].replace('X','23').replace('Y','24').replace('M','25'))<int(lpr1[0].replace('X','23').replace('Y','24').replace('M','25')):
 		line2=doc2.readline()
 		lpr2=line2.strip().split('\t')
 		continue
 	
-	if coint> 100:
-		break
+#finding similar chromosome
+	lpr1[0].replace('chr','').replace('X','23').replace('Y','24').replace('M','25')
+	lpr2[0].replace('chr','').replace('X','23').replace('Y','24').replace('M','25')
+	if int(lpr1[0])<int(lpr2[0]):
+		line1=doc1.readline()
+		lpr1=line1.strip().split('\t')
+		continue
+	if int(lpr2[0])<int(lpr1[0]):
+		line2=doc2.readline()
+		lpr2=line2.strip().split('\t')
+		continue
+	
+	#if coint> 100:
+	#	break
 		
 	if int(lpr1[0])<int(lpr2[0]):
 		i1+=1
@@ -48,7 +59,10 @@ while 0==0 :
 		i2+=1
 		continue
 		
-	if coint> 100000:
-		break
+	#if coint> 100000:
+	#	break
 print(coint)
-		
+doc1.close()
+doc1_w.close()
+doc2.close()
+doc2_w.close()
