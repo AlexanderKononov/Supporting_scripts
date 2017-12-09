d00a8d17c41c739391eee458348fc727c83cda44
AlexanderKononov <kononov.nov@mail.ru>
2017-12-09 05:26:20 +0200

    clonViewer.py

--- clonViewer.py
+++ clonViewer.py
@@ -33,13 +33,37 @@ def get_data(fname, chrs=None):
             print 'unsupported file format'            
             return None
 
+def genome_from_dict (dictionary):
+	genome=np.zeros((1, len(dictionary[dictionary.keys()[0]][0])))
+	current_position=0
+	chromosome_bounds=[]
+	for key in dictionary.keys():
+		current_data=dictionary[key]
+		for pos in current_data:
+			pos[0]+=current_position
+			pos[2]+=current_position
+		current_position+=dictionary[key][-1][2]
+		chromosome_bounds.append(current_position)
+		genome = np.vstack((genome, current_data))
+	return genome , chromosome_bounds
 
+def offset_chr_ticks (chr_bounds):
+	ticker_coordinates=[0]
+	privious_position=0
+	if chr_bounds[0]==0:
+		chr_bounds.pop(0)
+	for i in range(len(chr_bounds)):
+		ticker_coordinates.append(privious_position+((chr_bounds[i]-privious_position)//2))
+		privious_position=chr_bounds[i]
+	return ticker_coordinates
+		
 
 ####################################################################################
 
 arg=sys.argv
 arg[1]=arg[1].split('.cna.subclone')[0]
 
+target_chr=5
 ####################################################################################
 
 cnafiles = glob.glob(arg[1]+".cna.subclone-?.txt")
@@ -48,23 +72,28 @@ fig, axes = plt.subplots(len(cnafiles), figsize=(20, 5), facecolor='w', edgecolo
 
 for sc,cnafile in enumerate(cnafiles):
     [cn, freq, post] = get_data(cnafile)
-    
     ax = axes[sc]
-    
-    data = post[1][:,3:].T
-    x = post[1][:,0]
+
+    genome, chromosome_bounds = genome_from_dict(post)
+    data = genome[:,3:].T
+    x = genome[:,0]
     y = np.arange(data.shape[0] + 1)
     
     im = ax.pcolor(x, y, data, cmap=plt.cm.BuGn)
-
+    for ch in chromosome_bounds:
+        ax.axvline(x=ch, color='0.75')
+    
     # remove blank columns
     ax.set_xlim( (x.min(), x.max()) )
     ax.set_ylim( (y.min(), y.max()) )
-    
+    #print(offset_chr_ticks(chromosome_bounds))
+    #print(range(len(chromosome_bounds)))
+    #print(['0']+post.keys())
     # set ticks
-    scale = 1e3   # label and rescale x-axis   
-    ticks = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x/scale))   
-    ax.xaxis.set_major_formatter(ticks)
+    #scale = 1e3   # label and rescale x-axis
+    ax.xaxis.set_major_locator(ticker.FixedLocator(offset_chr_ticks(chromosome_bounds)))
+    #ticks = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x/scale))
+    ax.xaxis.set_major_formatter(ticker.FixedFormatter(['0']+['chr'+str(int(ch)) for ch in post.keys()]))
     
     ax.set_yticks(np.arange(data.shape[0]) + .5)
     ax.set_yticklabels(y)
@@ -93,6 +122,7 @@ plt.show()
 
 ####################################################################################
 
+
 baffiles = glob.glob(arg[1]+".baf.subclone-?.txt")
 
 fig, axes = plt.subplots(len(baffiles), figsize=(20, 5), facecolor='w', edgecolor='k', sharex=True, sharey=True)
@@ -101,20 +131,24 @@ for sc,baffile in enumerate(baffiles):
     [baf, freq, post] = get_data(baffile)
     ax = axes[sc]
     
-    data = post[1][:,3:].T
-    x = post[1][:,0]
+    genome, chromosome_bounds = genome_from_dict(post)
+    data = genome[:,3:].T
+    x = genome[:,0]
     y = np.arange(data.shape[0] + 1)
     
     im = ax.pcolor(x, y, data, cmap=plt.cm.PuBu)
+    for ch in chromosome_bounds:
+        ax.axvline(x=ch, color='0.75')
     
     # remove blank columns
     ax.set_xlim( (x.min(), x.max()) )
     ax.set_ylim( (y.min(), y.max()) )
     
     # set ticks
-    scale = 1e3   # label and rescale x-axis   
-    ticks = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x/scale))   
-    ax.xaxis.set_major_formatter(ticks)
+    #scale = 1e3   # label and rescale x-axis   
+    #ticks = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x/scale))
+    ax.xaxis.set_major_locator(ticker.FixedLocator(offset_chr_ticks(chromosome_bounds)))   
+    ax.xaxis.set_major_formatter(ticker.FixedFormatter(['0']+['chr'+str(int(ch)) for ch in post.keys()]))
     
     ax.set_yticks(np.arange(data.shape[0]) + .5)
     ax.set_yticklabels(y)
