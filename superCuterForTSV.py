948bf5743db36f97df743888b0000f1c91472fa6
AlexanderKononov <kononov.nov@mail.ru>
2018-04-29 07:15:10 +0300

    add  superCuterToTSV.py

---
+++ superCuterForTSV.py
@@ -0,0 +1,43 @@
+import sys
+
+arg = sys.argv
+rate = 1
+if len(arg) == 4:
+	rate = float(arg[3])
+f_r = open(arg[1], 'r')
+currentName = ''
+data = {}
+heder = f_r.readline()
+line = f_r.readline()
+while line != '':
+	l = line.strip().split('\t')
+	if l[0].find(arg[2]) == -1:
+		line = f_r.readline()
+		continue
+	name = l[0].strip().split(':')[0]
+	if name != currentName:
+		currentName = name
+		data[name] = []
+	l[0] = l[0].replace(name + ':', '')
+	data[name].append(l)
+	line = f_r.readline()
+f_r.close()
+
+positions = []
+for i in range(len(data[currentName])):
+	zeros = 0
+	for j in data.keys():
+		if int(data[j][i][2]) == 0:
+			zeros += 1
+	if zeros < len(data)/rate:
+		positions.append(data[currentName][i][0])
+
+for i in data.keys():
+	f_w = open(i + '_sml.tsv', 'w')
+	f_w.write(heder)
+	for j in data[i]:
+		if j[0] in positions:
+			for l in j:
+				f_w.write(l + '\t')
+			f_w.write('\n')
+	f_w.close()
